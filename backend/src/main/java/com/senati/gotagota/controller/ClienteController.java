af95fe7305bedadd2d5f6354a5df58b0c333fc54
c15497349-cmyk <c15497349@gmail.com>
2026-04-11 16:32:43 -0500

    senati

--- backend/src/main/java/com/senati/gotagota/controller/ClienteController.java
+++ backend/src/main/java/com/senati/gotagota/controller/ClienteController.java
@@ -3,38 +3,34 @@ package com.senati.gotagota.controller;
 
 import com.senati.gotagota.entity.Cliente;
 import com.senati.gotagota.service.ClienteService;
-import  org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import  org.springframework.web.bind.annotation.RequestMapping;
-import  org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 //3 ANOTACIONES
-//Indica que esta clase maneja peticiones HTTPS y DEVULEVE  JSON
+//Indica que esta clase maneja peticiones HTTPS y DEVUELVE JSON
 @RestController
-//Define la URL Base de todos los END-POINT de esta clase
+// Define la URL Base de todos los END-POINT de esta clase
 @RequestMapping("api/clientes")
-//Esta anotacion permite que el front-end puede llamar
-// Si no ponemos esto, el navegador bloquea las peticiones
-
+// Esta anotacion permite que el front-end pueda llamar a esta API
+// Si no ponemos esto, el navegador bloquea las peticiones por politicas CORS
 @CrossOrigin(origins = "*")
-
-
 public class ClienteController {
     //DECLARAMOS UNA VARIABLE DEFINIDA
-    //Inyectamos al servicio para acceder a la logica del negocio
-
+    // Inyectamos el servicio para acceder a la logica del negocio
     private final ClienteService clienteService;
-
-    public ClienteController(ClienteService clienteService) {
+    public ClienteController(ClienteService clienteService){
         this.clienteService = clienteService;
     }
-//GET /api /clientes -> devuelve todos los clientes en forma JSON
-
+    //GET /api/clientes -> devuelve todos los clientes en formato JSON
     @GetMapping
-    public List<Cliente> listar() {
-        return clienteService.listarTodos();
+    public List<Cliente> listar() {return clienteService.listarTodos();}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        clienteService.eliminarCliente(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,55 +1,118 @@
+
 // EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch('http://localhost:8080/api/clientes')
+  fetch("http://localhost:8080/api/clientes")
+    .then((response) => response.json())
+    .then((data) => {
+      // DOM -> <tbody id="table-cliente">
+      const elemento = document.getElementById("table-cliente");
+      for (let i = 0; i < data.length; i++) {
+        //data[i], muestra en forma de array
+        let cliente = data[i];
+        // alt + 96
+        let fila = `
+                            <tr>
+                            <td>${cliente.id}</td>
+                            <td>${cliente.nombre}</td>
+                            <td>${cliente.apellido}</td>
+                            <td>${cliente.dni}</td>
+                            <td>${cliente.telefono}</td>
+                            <td>${cliente.direccion}</td>
+                            <td> 
+                                <button class="btn btn-outline-primary me-2">
+                                    <i class="fas fa-edit"></i> Editar
+                                </button>
+                                <button id="btnEliminar" data-idcliente = ${cliente.id} class="btn btn-outline-danger">
+                                    <i class="fas fa-trash"></i> Eliminar
+                                </button>
+                            </td>
+                            </tr>                
+                           `;
+        elemento.innerHTML += fila;
+      }
+    });
 
-        .then(response => response.json())
+});
 
-        .then(data => {
-
-            // console.log('Datos:', data);
-
-            const elemento = document.getElementById("table-cliente")
-
-
-
-            for (let i = 0; i < 10; i++) {
-
-                let cliente = data[i]
-
-                let fila = `
-                 <tr>
-                 <td>${cliente.id}</td>
-                 <td>${cliente.nombre}</td>
-                 <td>${cliente.apellido}</td> 
-                 <td>${cliente.dni}</td> 
-                 <td>${cliente.telefono}</td>   
-                 <td>${cliente.direccion}</td> 
-                 <td>
-                     <button class="btn btn-outline-primary me-2">
-                  <i class="fas fa-edit"></i> Editar
-                </button>
-
-                 <button class="btn btn-outline-danger">
-                <i class="fas fa-trash-alt"></i> Eliminar
-                </button>
-                </td>
-                 </tr>
-           `
-                elemento.innerHTML += fila
-
-
-                //console.log(cliente) //Muestre los resultados en consola
-
-            }
-
-
-
-            // elemento.innerHTML = JSON.stringify(data);
-
-            //console.log(elemento)
-
+// EVENTO DE CLICK EN JAVASCRIPT
+//Creamos una variable que almacene el DOM de ese elemento del boton
+document.addEventListener("click", function (e) {
+  const btnDelete = e.target.closest("#btnEliminar");
+    if (btnDelete) { //TRUE o 1
+        alert("Eliminando...");
+        const id = btnDelete.dataset.idcliente;
+        //console.log(id) para en consola que ID es nada mas
+       // fetch(`http://localhost:8080/api/clientes/${id}`, {
+        fetch("http://localhost:8080/api/clientes/"+id, {
+          method: 'DELETE'
         })
+        .then(response => {
+            if (response.ok) {
+              alert('Cliente eliminado correctamente');
+              location.reload(); // Recargar la página para reflejar los cambios
+            } else {
+              alert('Error al eliminar el cliente');
+        }})
+    }
+});
 
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  fetch("http://localhost:8080/api/clientes")
+    .then((response) => response.json())
+    .then((data) => {
+      // DOM -> <tbody id="table-cliente">
+      const elemento = document.getElementById("table-cliente");
+      for (let i = 0; i < data.length; i++) {
+        //data[i], muestra en forma de array
+        let cliente = data[i];
+        // alt + 96
+        let fila = `
+                            <tr>
+                            <td>${cliente.id}</td>
+                            <td>${cliente.nombre}</td>
+                            <td>${cliente.apellido}</td>
+                            <td>${cliente.dni}</td>
+                            <td>${cliente.telefono}</td>
+                            <td>${cliente.direccion}</td>
+                            <td> 
+                                <button class="btn btn-outline-primary me-2">
+                                    <i class="fas fa-edit"></i> Editar
+                                </button>
+                                <button id="btnEliminar" data-idcliente = ${cliente.id} class="btn btn-outline-danger">
+                                    <i class="fas fa-trash"></i> Eliminar
+                                </button>
+                            </td>
+                            </tr>                
+                           `;
+        elemento.innerHTML += fila;
+      }
+    });
+
+});
+
+// EVENTO DE CLICK EN JAVASCRIPT
+//Creamos una variable que almacene el DOM de ese elemento del boton
+document.addEventListener("click", function (e) {
+  const btnDelete = e.target.closest("#btnEliminar");
+    if (btnDelete) { //TRUE o 1
+        alert("Eliminando...");
+        const id = btnDelete.dataset.idcliente;
+        //console.log(id) para en consola que ID es nada mas
+       // fetch(`http://localhost:8080/api/clientes/${id}`, {
+        fetch("http://localhost:8080/api/clientes/"+id, {
+          method: 'DELETE'
+        })
+        .then(response => {
+            if (response.ok) {
+              alert('Cliente eliminado correctamente');
+              location.reload(); // Recargar la página para reflejar los cambios
+            } else {
+              alert('Error al eliminar el cliente');
+        }})
+    }
 });
